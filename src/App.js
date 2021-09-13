9c3c1da26c7688e43c7809c4b3a44d81a38133e7
Jblack262 <71471784+Jblack262@users.noreply.github.com>
2021-09-13 15:20:45 -0700

    loading and error handling

--- src/App.js
+++ src/App.js
@@ -6,11 +6,25 @@ const url = "https://course-api.com/react-tours-project";
 
 function App() {
   const [places, setPlaces] = useState([]);//sets items to an empty array before putting api info inside it
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   const getInfo = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    setPlaces(data);
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((places) => {
+        setPlaces(places);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error))
     // console.log(data);
   }
   
@@ -25,6 +39,12 @@ function App() {
 
   return (
     <div className="App">
+      {isLoading && (
+        <h1>LOADING</h1>
+      )}
+      {isError && (
+        <h1>THERE HAS BEEN A BREACH</h1>
+      )}
       {places.map((place) => {
         // console.log(place)
         return (
